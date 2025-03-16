ea0cda5b1f3de48819b9b96368b15598572339df
Zero <102381@glr.nl>
2025-03-16 14:50:45 +0000

    boxes werken nu met redirecte naar een andere pagina header press me niet meer nodig

---
+++ html/script/redirect.js
@@ -0,0 +1,11 @@
+document.getElementById("boxing").addEventListener("click", function () {
+    window.location.href = "boxing-quiz.html";
+});
+
+document.getElementById("skating").addEventListener("click", function () {
+    window.location.href = "sk8.html";
+});
+
+document.getElementById("third").addEventListener("click", function () {
+    window.location.href = "third.html";
+});
