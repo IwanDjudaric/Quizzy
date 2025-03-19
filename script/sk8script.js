635eec00e08e5b39893246b65d7783f46aa84794
Iwan <101724@glr.nl>
2025-03-19 15:12:49 +0000

    made sk8 styled and functional for the most part

--- script/sk8script.js
+++ script/sk8script.js
@@ -1,1 +1,10 @@
-//start code
+window.onload = function () {
+  console.log("hi");
+  document.getElementById("questiontext").innerHTML = `What brand of deck is this?`;
+  let image = document.getElementById("image");
+  image.src = "/html/style/images/antihero.png";
+  document.getElementById("option1").innerHTML = `Baker`
+  document.getElementById("option2").innerHTML = `Anti-hero`
+  document.getElementById("option3").innerHTML = `Girl`
+  
+};
