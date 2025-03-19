bb2f58e17dfb141e858a4dc86e12a337bd9d64d1
Rasim102797 <102797@glr.nl>
2025-03-19 12:52:23 +0000

    style en java

---
+++ script/surfen.js
@@ -0,0 +1,15 @@
+function checkAnswers() {
+    let score = 0;
+    const answers = {
+        q1: "offshore",
+        q2: "onderwater",
+        q3: "schuim",
+        q4: "opstaan",
+        q5: "nose",
+        q6: "voorrang",
+        q7: "rotsen",
+        q8: "koord",
+        q9: "golfreeks",
+        q10: "grip"
+    };
+}
